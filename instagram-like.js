6e0e74cb932c942921e27a1a30c84158119ff177
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-02-29 23:33:57 +0530

    Update

--- instagram-like.js
+++ instagram-like.js
@@ -7,6 +7,10 @@ card.addEventListener('dblclick', function () {
     like.style.opacity = 0.8;
 
     setTimeout(() => {
+        like.style.opacity = 0;
+    }, 1000);
+
+    setTimeout(() => {
         like.style.transform = "translate(-50%, -50%) scale(0)";
     }, 2000);
 });
